af8907472e798b22836abe66690d8beabb5ca2fd
slail <sbillicko@gmail.com>
2022-12-30 21:03:37 -0500

    added options to make chrome run headless

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ import time
 class PythonOrgSearchTTest(unittest.TestCase):
 
     def setUp(self):
-        # options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
-        # browser = webdriver.Chrome(options=options)
-        self.driver = webdriver.Chrome(
-            service=Service('/usp/Local/bin/chromedriver'))
-        self.driver.maximize_window()
+
+        # !! Options to make it HEADLESS CHROME, run in background !!
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome(service=Service(
+            '/usp/Local/bin/chromedriver'), options=options)
+
+        # !! Options to make it NOT headless !!
+
+        # self.driver = webdriver.Chrome(
+        #     service=Service('/usp/Local/bin/chromedriver'))
+        # self.driver.maximize_window()
+
         self.driver.get("https://www.booking.com/")
 
     def test_One(self):
